aba39d55b5ecec3acb293dc05dacdeab0c662d3b
Mahaaltaki <mahaaltaki103@gmail.com(i made the git account on it )>
2025-06-12 15:23:34 +0300

    secondUpdate

---
+++ Data/AppDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using kalamon_University.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+        // 🔁 عدل الاتصال حسب إعداداتك:
+        optionsBuilder.UseSqlServer("Server=localhost;Database=UniversityDB;Trusted_Connection=True;");
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+
+}
